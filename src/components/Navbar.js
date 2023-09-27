0de40c2f22ce2b0a9684e3b79e31fed007ca3f7d
Sbosh 11 <sbosh11@gmail.com>
2023-09-27 15:29:06 +0200

    Nav edit auto close

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,11 +9,17 @@ function Navbar() {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <>
       <nav className={`nav ${isMenuOpen ? "nav-open" : ""}`}>
         <div className="nav-header">
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={closeMenu}>
+            Home
+          </Link>
           <button className="nav-toggle" onClick={toggleMenu}>
             <span></span>
             <span></span>
@@ -22,8 +28,12 @@ function Navbar() {
         </div>
 
         <div className={`nav-menu ${isMenuOpen ? "open" : ""}`}>
-          <Link to="/accordion">Snapdocs Accordion</Link>
-          <Link to="/calculator">Calculator</Link>
+          <Link to="/accordion" onClick={closeMenu}>
+            Snapdocs Accordion
+          </Link>
+          <Link to="/calculator" onClick={closeMenu}>
+            Calculator
+          </Link>
         </div>
       </nav>
       <Outlet />
