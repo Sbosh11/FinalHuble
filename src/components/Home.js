3f2b305a992649320a9c490052f4c56f172f1020
Sibo <sbosh11@gmail.com>
2024-04-23 12:01:33 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home">
-     <h1>BY SIBONGILE THANJEKWAYO/h1>
+    <h1> BY SIBONGILE THANJEKWAYO </h1>
       {/*  <p>By: Sibongile Thanjekwayo</p>*/}
     </div>
   );
